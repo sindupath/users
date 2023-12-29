6a3083c9ab26343c64ca9e1981b41899d6b573a1
sindupath <sindupathofficial@gmail.com>
2023-12-29 12:09:35 +0530

    good

--- js/script.js
+++ js/script.js
@@ -1,13 +1,32 @@
 'use strict';
 
+// modal variables
+const modal = document.querySelector('[data-modal]');
+const modalCloseBtn = document.querySelector('[data-modal-close]');
+const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
+
+// modal function
+const modalCloseFunc = function () { modal.classList.add('closed') }
+
+// modal eventListener
+modalCloseOverlay.addEventListener('click', modalCloseFunc);
+modalCloseBtn.addEventListener('click', modalCloseFunc);
+
+
+
+
+
 // notification toast variables
 const notificationToast = document.querySelector('[data-toast]');
 const toastCloseBtn = document.querySelector('[data-toast-close]');
 
 // notification toast eventListener
 toastCloseBtn.addEventListener('click', function () {
-    notificationToast.classList.add('closed');
+  notificationToast.classList.add('closed');
 });
+
+
+
 
 
 // mobile menu variables
@@ -18,19 +37,19 @@ const overlay = document.querySelector('[data-overlay]');
 
 for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
 
-    // mobile menu function
-    const mobileMenuCloseFunc = function () {
-        mobileMenu[i].classList.remove('active');
-        overlay.classList.remove('active');
-    }
+  // mobile menu function
+  const mobileMenuCloseFunc = function () {
+    mobileMenu[i].classList.remove('active');
+    overlay.classList.remove('active');
+  }
 
-    mobileMenuOpenBtn[i].addEventListener('click', function () {
-        mobileMenu[i].classList.add('active');
-        overlay.classList.add('active');
-    });
+  mobileMenuOpenBtn[i].addEventListener('click', function () {
+    mobileMenu[i].classList.add('active');
+    overlay.classList.add('active');
+  });
 
-    mobileMenuCloseBtn[i].addEventListener('click', mobileMenuCloseFunc);
-    overlay.addEventListener('click', mobileMenuCloseFunc);
+  mobileMenuCloseBtn[i].addEventListener('click', mobileMenuCloseFunc);
+  overlay.addEventListener('click', mobileMenuCloseFunc);
 
 }
 
@@ -44,26 +63,26 @@ const accordion = document.querySelectorAll('[data-accordion]');
 
 for (let i = 0; i < accordionBtn.length; i++) {
 
-    accordionBtn[i].addEventListener('click', function () {
+  accordionBtn[i].addEventListener('click', function () {
 
-        const clickedBtn = this.nextElementSibling.classList.contains('active');
+    const clickedBtn = this.nextElementSibling.classList.contains('active');
 
-        for (let i = 0; i < accordion.length; i++) {
+    for (let i = 0; i < accordion.length; i++) {
 
-            if (clickedBtn) break;
+      if (clickedBtn) break;
 
-            if (accordion[i].classList.contains('active')) {
+      if (accordion[i].classList.contains('active')) {
 
-                accordion[i].classList.remove('active');
-                accordionBtn[i].classList.remove('active');
+        accordion[i].classList.remove('active');
+        accordionBtn[i].classList.remove('active');
 
-            }
+      }
 
-        }
+    }
 
-        this.nextElementSibling.classList.toggle('active');
-        this.classList.toggle('active');
+    this.nextElementSibling.classList.toggle('active');
+    this.classList.toggle('active');
 
-    });
+  });
 
 }
